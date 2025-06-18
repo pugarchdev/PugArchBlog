815bc7994c4893d6008257c36dd985c7403384ca
Kartik Kanzode <kartikkanzode@gmail.com>
2025-06-18 17:39:38 +0530

    Update page.jsx

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -294,7 +294,7 @@ const WritePage = () => {
                   setMedia("");
                 }}
               >
-                
+                x
               </button>
             </div>
            <Image
