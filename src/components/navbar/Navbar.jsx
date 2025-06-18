413539c8828d774b680fa9ee6be3618ac87805a0
Kartik Kanzode <kartikkanzode@gmail.com>
2025-06-18 18:07:24 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <Link href="/" className={styles.link}> <div className={styles.logo}>PugArch Blog</div></Link>
+      <Link href="/" className={styles.logoLink}> <div className={styles.logo}>PugArch Blog</div></Link>
      
       <div className={styles.links}>
         <ThemeToggle />
